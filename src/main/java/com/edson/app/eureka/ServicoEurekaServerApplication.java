8046c8669bdbe0e63c40df7cde3f17509bb19271
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-03-10 11:21:29 -0300

    Start project
    Configure eureka server

---
+++ src/main/java/com/edson/app/eureka/ServicoEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.edson.app.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServicoEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoEurekaServerApplication.class, args);
+	}
+
+}
